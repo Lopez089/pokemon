ae5e414f6030c276528710d4d6b9a74bdb5ccaf0
LopezDev <aragonlopezjuan87@gmail.com>
2023-12-07 07:44:48 +0100

    first comit

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+
+
+
+const App = () => {
+  const [pokemons, setPokemons] = useState([])
+
+  const getPokemon = async (limit = 20) => {
+    const baseUrl = 'https://pokeapi.co/api/v2'
+
+    const res = await fetch(`${baseUrl}/pokemon?limit=${limit}&offset=0`)
+    const data = await res.json()
+
+    const promise = data.results.map(async (pokemon) => {
+      const res = await fetch(pokemon.url)
+      const data = await res.json()
+      return data
+    })
+
+    const allPokemon = await Promise.all(promise)
+
+    const promiseDataSpecies = allPokemon.map(async (pokemon) => {
+      const res = await fetch(pokemon.species.url)
+      const data = await res.json()
+      return {
+        id: pokemon.id,
+        name: pokemon.name,
+        color: data.color.name,
+        img: pokemon.sprites.front_default
+      }
+    })
+
+    const allPokemonData = await Promise.all(promiseDataSpecies)
+
+    setPokemons(allPokemonData)
+  }
+  useEffect(() => {
+    getPokemon()
+  }, []);
+
+
+
+  return (
+    <main>
+      <h1>Pokemon</h1>
+      <section className="container">
+        {
+          pokemons.map(pokemon => {
+            return (
+              <article className='card' key={pokemon.id} style={{ 'backgroundColor': pokemon.color }}>
+                <img src={pokemon.img} alt={pokemon.name} />
+                <h3>{pokemon.name}</h3>
+                <p>{
+                  pokemon.id < 10 ? `00${pokemon.id}` :
+                    pokemon.id < 100 ? `0${pokemon.id}` :
+                      pokemon.id}</p>
+              </article >
+            )
+          })
+        }
+
+      </section>
+    </main>
+  )
+}
+
+export default App
